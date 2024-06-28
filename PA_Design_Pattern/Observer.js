3261314cf02e7cb4c0cd9e0b3779c56830d159e2
Laura Daflon <83421434+LauraDaflon96@users.noreply.github.com>
2024-06-28 20:05:13 -0300

    Update Observer.js

--- PA_Design_Pattern/Observer.js
+++ PA_Design_Pattern/Observer.js
@@ -1,7 +1,3 @@
-// The Observer Design Pattern is a behavioral design pattern that defines a 
-// one-to-many dependency between objects so that when one object (the subject) 
-// changes state, all its dependents (observers) are notified and updated automatically.
-
 class Observable {
     constructor() {
         this.observers = [];
